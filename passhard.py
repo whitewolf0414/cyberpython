572c641aa9c27cc127b028a0d280c05d940403f4
H@4! <mechanic0414@gmail.com>
2026-01-19 23:35:45 +0530

    initial commit - password strength checker

---
+++ passhard.py
@@ -0,0 +1,8 @@
+#making a password strength checker for my first cybersecurity program using python
+
+import time
+print("create a password here")
+time.sleep(1)
+a=input("type your password:")
+x=a
+print("entered password is ",x) # made a working input and output for password checker 1st attempt
