61f8b2bb94b109a4f5d9b8a51f8d94fc959e13b2
H@4! <mechanic0414@gmail.com>
2026-01-22 01:07:00 +0530

    added new updates like checking numbers symbols and length of the password

--- passhard.py
+++ passhard.py
@@ -1,19 +1,19 @@
 #making a password strength checker for my first cybersecurity program using python
 
 import time
+import string
 print("create a password here")
 time.sleep(1)
 a=input("type your password:")
-
-passw = "a"
-has_capital = False
-for passw in a:
-         if passw.isupper():
-              has_capital = True
-              break
-if has_capital:
-             print("password is " , a)
+passw = a  # putting the value into it
+if any(char.isupper() for char in passw) and any(char.isdigit() for char in passw) and any(char in string.punctuation  for char in passw) and  len(passw) >= 8:
+     print("your password has been set") #learn to use the function ,methods and operators
 else:
-              print("entered password cant be created") # check whether the password contains capital letter
+    print(" password doesnt meet the requirements")
+
+
+
+
+
 
 
