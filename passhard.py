45fc7cbea6eaa3d8485db1838c503603932a1369
H@4! <mechanic0414@gmail.com>
2026-01-22 02:05:24 +0530

    added feature to check lowercase also alternate outputs if the criteria never meet expectation

--- passhard.py
+++ passhard.py
@@ -3,13 +3,28 @@
 import time
 import string
 print("create a password here")
-time.sleep(1)
-a=input("type your password:")
-passw = a  # putting the value into it
-if any(char.isupper() for char in passw) and any(char.isdigit() for char in passw) and any(char in string.punctuation  for char in passw) and  len(passw) >= 8:
-     print("your password has been set") #learn to use the function ,methods and operators
+time.sleep(.25)                  # added delay so the program output will look good
+a = input("type your password:")
+passw = a
+hasuppercase = any(a.isupper() for a in passw)
+haslowercase = any(a.islower() for a in passw)
+hasdigit = any(a.isdigit() for a in passw)
+hassymbols = any(a in string.punctuation for a in passw)
+checklength = len(passw) >=8
+if hasuppercase and haslowercase and hasdigit and hassymbols and checklength:
+    print("password is has been created")
 else:
-    print(" password doesnt meet the requirements")
+    if not hasuppercase:
+        print("capital letter is not included")
+    if not haslowercase:
+        print("lowercase letter is not included")
+    if not hasdigit:
+        print("digit is not included")
+    if not hassymbols:
+        print("symbols is not included")
+    if not checklength:
+        print("password length is short")
+
 
 
 
